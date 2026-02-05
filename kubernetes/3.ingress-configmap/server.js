69c470a5a28821ac409f90a801f43ff45578f915
arulvalananto <arulvalananto@gmail.com>
2026-02-05 13:37:20 +0530

    Add liveness and readiness probes to Node.js application

--- kubernetes/3.ingress-configmap/server.js
+++ kubernetes/3.ingress-configmap/server.js
@@ -3,6 +3,13 @@ const app = express();
 
 const PORT = 3000;
 
+// Simulate slow startup
+let ready = false;
+setTimeout(() => {
+    ready = true;
+    console.log('App is ready');
+}, 10000); // 10 seconds
+
 app.get('/', (req, res) => {
     res.send(
         `${process.env.MESSAGE} | Env: ${process.env.APP_ENV} | Pod: ${process.env.HOSTNAME}`,
@@ -13,6 +20,17 @@ app.get('/cpu', (req, res) => {
     const start = Date.now();
     while (Date.now() - start < 300) {} // burn CPU ~300ms
     res.send(`CPU burned by ${process.env.HOSTNAME}`);
+});
+
+// Readiness probe
+app.get('/ready', (req, res) => {
+    if (ready) return res.sendStatus(200);
+    res.sendStatus(503);
+});
+
+// Liveness probe
+app.get('/health', (req, res) => {
+    res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
