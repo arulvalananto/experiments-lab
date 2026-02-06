c3e18a9f319c0266547b20877472a986898f435d
arulvalananto <arulvalananto@gmail.com>
2026-02-06 08:32:34 +0530

    Add Kubernetes configuration and Node.js application for security isolation demo
    - Created package.json for Node.js application with Express dependency.
    - Added secret.yaml to define Kubernetes secret for database credentials.
    - Implemented server.js for a simple Express application with readiness and liveness probes.
    - Added service.yaml to expose the Node.js application as a ClusterIP service.

---
+++ kubernetes/4.security-isolation/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+// Simulate slow startup
+let ready = false;
+setTimeout(() => {
+    ready = true;
+    console.log('App is ready');
+    console.log('DB_USERNAME LOADED', !!process.env.DB_USERNAME);
+}, 10000); // 10 seconds
+
+app.get('/', (req, res) => {
+    res.send(
+        `${process.env.MESSAGE} | Env: ${process.env.APP_ENV} | Pod: ${process.env.HOSTNAME} | DB User: ${process.env.DB_USERNAME}`,
+    );
+});
+
+app.get('/cpu', (req, res) => {
+    const start = Date.now();
+    while (Date.now() - start < 300) {} // burn CPU ~300ms
+    res.send(`CPU burned by ${process.env.HOSTNAME}`);
+});
+
+// Readiness probe
+app.get('/ready', (req, res) => {
+    if (ready) return res.sendStatus(200);
+    res.sendStatus(503);
+});
+
+// Liveness probe
+app.get('/health', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
