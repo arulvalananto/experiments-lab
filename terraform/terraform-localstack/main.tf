60363a551b5c7ee1c9795b6c544d46e5ac00a804
arulvalananto <arulvalananto@gmail.com>
2026-02-17 08:45:34 +0530

    feat(terraform): parameterize S3 bucket and DynamoDB table names, add environment tags

--- terraform/terraform-localstack/main.tf
+++ terraform/terraform-localstack/main.tf
@@ -23,22 +23,30 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-local-bucket"
+  bucket = "${var.bucket_name}-${var.environment}"
+
+  tags = {
+    Environment = var.environment
+  }
 }
 
 resource "aws_s3_object" "my_object" {
   bucket  = aws_s3_bucket.my_bucket.id
   key     = "my-object"
-  content = "Hello from Terraform + LocalStack"
+  content = "Hello from ${var.environment}"
 }
 
 resource "aws_dynamodb_table" "my_table" {
-  name         = "my-local-table"
+  name         = "${var.dynamodb_table_name}-${var.environment}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
   attribute {
     name = "id"
     type = "S"
+  }
+
+  tags = {
+    Environment = var.environment
   }
 }
---
+++ terraform/terraform-localstack/variables.tf
@@ -0,0 +1,14 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
