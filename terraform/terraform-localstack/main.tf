d85f21a8451f1ba5e2d33efc9dbf67c05203f11b
arulvalananto <arulvalananto@gmail.com>
2026-02-17 08:33:17 +0530

    feat(terraform): add initial Terraform configuration with AWS provider and resources

---
+++ terraform/terraform-localstack/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    s3       = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-local-bucket"
+}
+
+resource "aws_s3_object" "my_object" {
+  bucket  = aws_s3_bucket.my_bucket.id
+  key     = "my-object"
+  content = "Hello from Terraform + LocalStack"
+}
+
+resource "aws_dynamodb_table" "my_table" {
+  name         = "my-local-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
